5aa09913a8124affc20a76a07274161c50697546
tr20331ps <gomesw@gmail.com>
2022-05-23 09:11:28 -0300

    Versao final

--- lib/models/product.dart
+++ lib/models/product.dart
@@ -1,24 +1,24 @@
-class Product{
+import 'package:flutter/material.dart';
+
+class Product with ChangeNotifier {
   final String id;
-  final String title;
+  final String name;
   final String description;
   final double price;
   final String imageUrl;
   bool isFavorite;
 
-  Product(
-      {
-        required this.id,
-        required this.description,
-        required this.title,
-        required this.price,
-        required this.imageUrl,
-        this.isFavorite = false,
-      });
+  Product({
+    required this.id,
+    required this.name,
+    required this.description,
+    required this.price,
+    required this.imageUrl,
+    this.isFavorite = false,
+  });
 
-  void toogleFavotite(){
+  void toggleFavorite() {
     isFavorite = !isFavorite;
+    notifyListeners();
   }
-
-
 }
